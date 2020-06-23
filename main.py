0e7f48fb57f6602b43783bfb0df01d5eb23801d9
Berkay Yüksel <berkayyuksel@protonmail.com>
2020-06-23 21:01:17 +0300

    fix

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from currencyConversion import currencyConversion as gui
 
 def main():
     obj = gui()
-    obj.createWindow("curency conversion bitch","400x100")
+    obj.createWindow("Currency Conversion","400x100")
     obj.initialize()
     
 if __name__ == "__main__":
